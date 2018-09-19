497e8eb0f69fe59ae6188b28dd22045f21d91f58
Yung Ankka <42602455+ogankka@users.noreply.github.com>
2018-09-19 13:38:21 +0300

    some changes

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-//Javscript kommentar
+$(document).ready(function(){
+	$("p").addClass("intro");
+	var uppg4width = $("#uppg4-box").width();
+	var uppg4height = $("#uppg4-box").height();
+	
+	$("#uppg4-box").html("<p>bredd: " + uppg4width + " px</p><p>höjd: " + uppg4height + " px</p>");
+	//$("#uppg4-box").heigth();
+	$("#uppg5-box").show('slow');
+	$("#show").click(function(){
+    $("#uppg6-box").show();
+});
+});
